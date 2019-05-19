a1f25fa96b9fb76a7e15fabdc7263aaf36e50d27
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-19 10:03:24 -0700

    try complex types

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /* Complex types */
+  // typle: a type of array that you can insert mulple types
+  let aTuple: [string, number] = ['test', 123];
+  // enum
+  enum Codes {
+    first = 1,
+    second = 2
+  }
+  // any
+  let firstName: any = 'Manny';
+  // void
+  const warning = (): void => {
+    console.log('warning!');
+  };
+
+  /* Simple types */
   // let firstValue: string = 'Manny';
   // let firstValue: number = 34;
   // let firstValue: boolean = true;
   // let firstValue: number[] = [2, 3, 4];
-  let firstValue: Array<string> = ['2', '3', '4'];
+  // let firstValue: Array<string> = ['2', '3', '4'];
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          The value {firstValue} is of {typeof firstValue} type!
+          The value {aTuple[1]} is of {typeof aTuple[1]} type!
+          <br />
+          The value {firstName} is of {typeof firstName} type!
+          <br />
+          The value {warning} is of {typeof warning} type!
+          {/* The value {firstValue} is of {typeof firstValue} type! */}
         </p>
       </header>
     </div>
