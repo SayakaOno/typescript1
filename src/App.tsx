33b805642a4f0456b1427a300a8b2843d22c1346
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-19 10:32:24 -0700

    add interface for props

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import Message from './message';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Message message='This is a simple message' />
-      </header>
-    </div>
-  );
+class App extends React.Component<any> {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <Message name='Manny' message='this is a simple message' />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
